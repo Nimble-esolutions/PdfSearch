464a61624defe4353784bf340f22096aa0cf24cc
Nimble-esolutions <86198548+Nimble-esolutions@users.noreply.github.com>
2025-10-18 23:17:06 +0530

    Update start.sh
    gunicorn worker timeout 300

--- start.sh
+++ start.sh
@@ -69,8 +69,8 @@ echo "------------------------------------------------------------"
 echo "🔥 Starting Gunicorn..."
 exec gunicorn \
     --bind 0.0.0.0:8000 \
-    --workers 3 \
-    --timeout 120 \
+    --workers 4 \
+    --timeout 300 \
     --access-logfile - \
     --error-logfile - \
     flowdocs.wsgi:application
