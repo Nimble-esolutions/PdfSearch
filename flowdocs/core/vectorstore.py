d24e1b8e8ad9fc565d56d137d68c91bf0d7b959b
Nimble-esolutions <86198548+Nimble-esolutions@users.noreply.github.com>
2025-11-07 14:08:38 +0530

    Switch embedding function to OpenAI model
    embedding_func

--- flowdocs/core/vectorstore.py
+++ flowdocs/core/vectorstore.py
@@ -1,14 +1,23 @@
 # vectorstore.py
 import chromadb
 from chromadb.utils import embedding_functions
+from django.conf import settings
 
 # Persistent DB (saves on disk)
 client = chromadb.PersistentClient(path="chroma_db")
 
 # Embedding model (you can later switch to OpenAI or local embeddings)
-embedding_func = embedding_functions.SentenceTransformerEmbeddingFunction(
-    model_name="all-MiniLM-L6-v2"
+# embedding_func = embedding_functions.SentenceTransformerEmbeddingFunction(
+#     model_name="all-MiniLM-L6-v2"
+# )
+
+
+#START-Kunjika
+embedding_func = embedding_functions.OpenAIEmbeddingFunction(
+    api_key=settings.OPENAI_API_KEY,
+    model_name="text-embedding-3-large"
 )
+#END-Kunjika
 
 # Create collection (like a table in SQL)
 pdf_collection = client.get_or_create_collection(
