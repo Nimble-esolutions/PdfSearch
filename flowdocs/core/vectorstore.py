aec2529aa056a4aeba58be48375b3ba631db6b95
Nimble-esolutions <86198548+Nimble-esolutions@users.noreply.github.com>
2025-11-10 17:45:09 +0530

    Refactor embedding functions and collections in vectorstore
    Updated Search logic

--- flowdocs/core/vectorstore.py
+++ flowdocs/core/vectorstore.py
@@ -3,24 +3,27 @@ import chromadb
 from chromadb.utils import embedding_functions
 from django.conf import settings
 
-# Persistent DB (saves on disk)
+# Persistent DB
 client = chromadb.PersistentClient(path="chroma_db")
 
-# Embedding model (you can later switch to OpenAI or local embeddings)
-# embedding_func = embedding_functions.SentenceTransformerEmbeddingFunction(
-#     model_name="all-MiniLM-L6-v2"
-# )
-
-
-#START-Kunjika
-embedding_func = embedding_functions.OpenAIEmbeddingFunction(
+# Embedding models
+embedding_large = embedding_functions.OpenAIEmbeddingFunction(
     api_key=settings.OPENAI_API_KEY,
     model_name="text-embedding-3-large"
 )
-#END-Kunjika
 
-# Create collection (like a table in SQL)
-pdf_collection = client.get_or_create_collection(
+embedding_small = embedding_functions.OpenAIEmbeddingFunction(
+    api_key=settings.OPENAI_API_KEY,
+    model_name="text-embedding-3-small"
+)
+
+# ✅ Collection must include embedding_function only here
+pdf_collection_large = client.get_or_create_collection(
     name="pdf_chunks",
-    embedding_function=embedding_func
+    embedding_function=embedding_large
+)
+
+pdf_collection_small = client.get_or_create_collection(
+    name="pdf_chunks_small",
+    embedding_function=embedding_small
 )
