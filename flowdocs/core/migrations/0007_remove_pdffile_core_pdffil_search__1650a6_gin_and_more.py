a0b86cf3b6e04a2005af00415c7f9a02293c6a0e
Nimble-esolutions <86198548+Nimble-esolutions@users.noreply.github.com>
2026-01-07 18:17:02 +0530

    Refactor migration to drop GIN index directly
    Removed the GIN index drop function and replaced it with a direct SQL command to drop the index. Other migration operations related to the 'pdffile' model have been commented out.

--- flowdocs/core/migrations/0007_remove_pdffile_core_pdffil_search__1650a6_gin_and_more.py
+++ flowdocs/core/migrations/0007_remove_pdffile_core_pdffil_search__1650a6_gin_and_more.py
@@ -1,41 +1,13 @@
-from django.db import migrations, models, connection
-
-
-def drop_gin_index_if_postgres(apps, schema_editor):
-    if connection.vendor == "postgresql":
-        schema_editor.execute(
-            "DROP INDEX IF EXISTS core_pdffil_search__1650a6_gin;"
-        )
-
+from django.db import migrations
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('core', '0006_pdffile_content_pdffile_search_vector_and_more'),
     ]
 
     operations = [
-        migrations.RunPython(
-            drop_gin_index_if_postgres,
-            migrations.RunPython.noop
-        ),
-
-        migrations.RemoveField(
-            model_name='pdffile',
-            name='content',
-        ),
-        migrations.RemoveField(
-            model_name='pdffile',
-            name='search_vector',
-        ),
-        migrations.AddField(
-            model_name='pdffile',
-            name='text_content',
-            field=models.TextField(blank=True, default=''),
-        ),
-        migrations.AlterField(
-            model_name='pdffile',
-            name='keywords',
-            field=models.JSONField(blank=True, default=list),
+        migrations.RunSQL(
+            sql="DROP INDEX IF EXISTS core_pdffil_search__1650a6_gin;",
+            reverse_sql=migrations.RunSQL.noop,
         ),
     ]
