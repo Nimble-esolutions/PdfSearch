3c158ed8c35c15817b2f6d59b7cf898c1fd55c7e
Nimble-esolutions <86198548+Nimble-esolutions@users.noreply.github.com>
2026-06-29 15:50:41 +0530

    Update welcome message link in views.py
    Changed Sample Question forms link.
    
    https://docs.google.com/document/d/1K4Z0RnRcQFXXDxxO10xFVjAbFRBXu7errbWbqtIK8qE/edit?usp=sharing

--- flowdocs/core/views.py
+++ flowdocs/core/views.py
@@ -464,7 +464,7 @@ def search_query(request):
         welcome_message = mark_safe(
             f"""
             {_(' I am Sahakar AI. Click here to learn how to questions to get correct answers.')}
-            <a href="https://docs.google.com/document/d/1BuDP903_0BWv2NYXcNorUD3DYQvPUPA7/edit?usp=sharing&ouid=105068040738847757452&rtpof=true&sd=true"
+            <a href="https://docs.google.com/document/d/1K4Z0RnRcQFXXDxxO10xFVjAbFRBXu7errbWbqtIK8qE/edit?usp=sharing"
                target="_blank"
                style="color: blue; text-decoration: underline;">
                {_('Click Here')}
